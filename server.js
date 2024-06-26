c74a09257871e45227ae851bf1a1655b7e290e7f
Michael <gt.gluck@gmail.com>
2024-06-26 10:40:56 +0300

    add: cors

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import express from 'express';
+import cors from 'cors'
 
 const app = express();
 
 const PORT = 3000;
 
+app.use(cors())
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.json({title: 'hello'});
 });
 
 app.listen(PORT, () => {
